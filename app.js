1a0c06ad0d930e5aa0e290844ebb5dfdfce32c53
21x37 <tylermok@outlook.com>
2019-04-27 14:18:39 -0700

    Initial commit 🚀

---
+++ app.js
@@ -0,0 +1,21 @@
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+
+
+class commentBot {
+    constructor(creds) {
+        this.creds = creds;
+        this.r = new Snoowrap(this.creds);
+        this.client = new Snoostorm(this.r);
+    };
+    streamComments(options = undefined, cb) {
+        if (options) {
+            const comments = this.client.CommentStream(options);
+            return comments.on('comment', (comment) => cb(comment) );
+        };
+        const comments = this.client.CommentStream();
+        return comments.on('comment', (comment) => cb(comment) );
+    };
+};
+
+module.exports = commentBot;
